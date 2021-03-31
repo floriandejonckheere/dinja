e06b7d27636535edb88369f3b1e154c1d8f3f472
Florian Dejonckheere <florian@floriandejonckheere.be>
2021-03-31 18:17:08 +0200

    Rewrite dependencies spec

--- spec/dependencies.rb
+++ spec/dependencies.rb
@@ -1,43 +1,134 @@
 # frozen_string_literal: true
 
-class MyService
-  attr_reader :input
+# rubocop:disable Lint/MissingSuper
+class Service
+  def call; end
+end
 
-  def initialize(input)
-    @input = input
+class WithArgument < Service
+  attr_reader :argument
+
+  def initialize(argument)
+    @argument = argument
   end
+end
 
-  def call
-    input.reverse
+class WithArguments < Service
+  attr_reader :argument_one, :argument_two
+
+  def initialize(argument_one, argument_two)
+    @argument_one = argument_one
+    @argument_two = argument_two
   end
 end
 
-# Simple
-register("simple") do
-  MyService.new("string")
+class WithSplat < Service
+  attr_reader :arguments
+
+  def initialize(*arguments)
+    @arguments = arguments
+  end
 end
 
-# With argument
-register("with_argument") do |arg|
-  MyService.new(arg)
+class WithKeyword < Service
+  attr_reader :keyword
+
+  def initialize(keyword:)
+    @keyword = keyword
+  end
 end
 
-# With arguments
-register("with_arguments") do |*args|
-  MyService.new(args)
+class WithKeywords < Service
+  attr_reader :keyword_one, :keyword_two
+
+  def initialize(keyword_one:, keyword_two:)
+    @keyword_one = keyword_one
+    @keyword_two = keyword_two
+  end
 end
 
-# With block
+class WithDoubleSplat < Service
+  attr_reader :keywords
+
+  def initialize(**keywords)
+    @keywords = keywords
+  end
+end
+
+class WithBlock < Service
+  attr_reader :block
+
+  def initialize(&block)
+    @block = block
+  end
+end
+
+class WithEverything < Service
+  attr_reader :argument_one, :argument_two, :keyword_one, :keyword_two, :block
+
+  def initialize(argument_one, argument_two, keyword_one:, keyword_two:, &block)
+    @argument_one = argument_one
+    @argument_two = argument_two
+    @keyword_one = keyword_one
+    @keyword_two = keyword_two
+    @block = block
+  end
+end
+
+class WithEverythingSplat < Service
+  attr_reader :arguments, :keywords, :block
+
+  def initialize(*arguments, **keywords, &block)
+    @arguments = arguments
+    @keywords = keywords
+    @block = block
+  end
+end
+# rubocop:enable Lint/MissingSuper
+
+register("service") do
+  Service.new
+end
+
+register("with_argument") do |argument|
+  WithArgument.new(argument)
+end
+
+register("with_arguments") do |argument_one, argument_two|
+  WithArguments.new(argument_one, argument_two)
+end
+
+register("with_splat") do |*arguments|
+  WithSplat.new(*arguments)
+end
+
+register("with_keyword") do |keyword:|
+  WithKeyword.new(keyword: keyword)
+end
+
+register("with_keywords") do |keyword_one:, keyword_two:|
+  WithKeywords.new(keyword_one: keyword_one, keyword_two: keyword_two)
+end
+
+register("with_double_splat") do |**keywords|
+  WithDoubleSplat.new(**keywords)
+end
+
 register("with_block") do |&block|
-  MyService.new(block.call)
+  WithBlock.new(&block)
 end
 
-# With arguments and block
-register("with_arguments_and_block") do |*args, &block|
-  MyService.new(args.map { |arg| block.call(arg) })
+register("with_everything") do |argument_one, argument_two, keyword_one:, keyword_two:, &block|
+  WithEverything.new(argument_one, argument_two, keyword_one: keyword_one, keyword_two: keyword_two, &block)
 end
 
-# Recursive
-register("recursive") do |arg|
-  resolve("with_argument", arg)
+register("with_everything_splat") do |*arguments, **keywords, &block|
+  WithEverythingSplat.new(*arguments, **keywords, &block)
+end
+
+register("recursive") do |argument_one, argument_two|
+  WithArguments.new(
+    resolve("with_argument", argument_one),
+    resolve("with_argument", argument_two),
+  )
 end
--- spec/dinja/dependencies_spec.rb
+++ spec/dinja/dependencies_spec.rb
@@ -6,40 +6,83 @@ RSpec.describe Dinja::Container do
 
   before { container.instance_eval File.read(Dinja.root.join("spec/dependencies.rb")) }
 
-  it "resolves simple dependencies" do
-    dependency = container.resolve("simple")
+  it "resolves a service" do
+    dependency = container.resolve("service")
 
-    expect(dependency.call).to eq "gnirts"
+    expect(dependency).to respond_to :call
   end
 
-  it "resolves dependencies with argument" do
-    dependency = container.resolve("with_argument", "string")
+  it "resolves a service with argument" do
+    dependency = container.resolve("with_argument", "my_argument")
 
-    expect(dependency.call).to eq "gnirts"
+    expect(dependency.argument).to eq "my_argument"
   end
 
-  it "resolves dependencies with arguments" do
-    dependency = container.resolve("with_arguments", "one", "two")
+  it "resolves a service with arguments" do
+    dependency = container.resolve("with_arguments", "my_argument_one", "my_argument_two")
 
-    expect(dependency.call).to eq %w(two one)
+    expect(dependency.argument_one).to eq "my_argument_one"
+    expect(dependency.argument_two).to eq "my_argument_two"
   end
 
-  it "resolves dependencies with block" do
-    dependency = container.resolve("with_block") { "string" }
+  it "resolves a service with splat" do
+    dependency = container.resolve("with_splat", "my_argument_one", "my_argument_two", "my_argument_three")
 
-    expect(dependency.call).to eq "gnirts"
+    expect(dependency.arguments).to eq %w(my_argument_one my_argument_two my_argument_three)
   end
 
-  it "resolves dependencies with arguments and block" do
-    dependency = container.resolve("with_arguments_and_block", "one", "two", &:reverse)
+  it "resolves a service with keyword" do
+    dependency = container.resolve("with_keyword", keyword: "my_keyword")
 
-    expect(dependency.call).to eq %w(owt eno)
+    expect(dependency.keyword).to eq "my_keyword"
+  end
+
+  it "resolves a service with keywords" do
+    dependency = container.resolve("with_keywords", keyword_one: "my_keyword_one", keyword_two: "my_keyword_two")
+
+    expect(dependency.keyword_one).to eq "my_keyword_one"
+    expect(dependency.keyword_two).to eq "my_keyword_two"
+  end
+
+  it "resolves a service with double splat" do
+    dependency = container.resolve("with_double_splat", keyword_one: "my_keyword_one", keyword_two: "my_keyword_two", keyword_three: "my_keyword_three")
+
+    expect(dependency.keywords).to eq keyword_one: "my_keyword_one", keyword_two: "my_keyword_two",
+                                      keyword_three: "my_keyword_three"
+  end
+
+  it "resolves a service with block" do
+    dependency = container.resolve("with_block") { "my_block" }
+
+    expect(dependency.block.call).to eq "my_block"
+  end
+
+  it "resolves a service with everything" do
+    dependency = container.resolve("with_everything", "my_argument_one", "my_argument_two", keyword_one: "my_keyword_one", keyword_two: "my_keyword_two") { "my_block" }
+
+    expect(dependency.argument_one).to eq "my_argument_one"
+    expect(dependency.argument_two).to eq "my_argument_two"
+
+    expect(dependency.keyword_one).to eq "my_keyword_one"
+    expect(dependency.keyword_two).to eq "my_keyword_two"
+
+    expect(dependency.block.call).to eq "my_block"
+  end
+
+  it "resolves a service with everything splat" do
+    dependency = container.resolve("with_everything_splat", "my_argument_one", "my_argument_two", "my_argument_three", keyword_one: "my_keyword_one", keyword_two: "my_keyword_two", keyword_three: "my_keyword_three") { "my_block" }
+
+    expect(dependency.arguments).to eq %w(my_argument_one my_argument_two my_argument_three)
+    expect(dependency.keywords).to eq keyword_one: "my_keyword_one", keyword_two: "my_keyword_two",
+                                      keyword_three: "my_keyword_three"
+    expect(dependency.block.call).to eq "my_block"
   end
 
   it "resolves recursive dependencies" do
-    dependency = container.resolve("recursive", "string")
+    dependency = container.resolve("recursive", "my_argument_one", "my_argument_two")
 
-    expect(dependency.call).to eq "gnirts"
+    expect(dependency.argument_one.argument).to eq "my_argument_one"
+    expect(dependency.argument_two.argument).to eq "my_argument_two"
   end
 end
 # rubocop:enable RSpec/FilePath
